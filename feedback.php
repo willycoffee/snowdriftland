57401349564012c74df1a39ce946290106996307
willycoffee <67596846+willycoffee@users.noreply.github.com>
2020-12-28 02:33:48 -0500

    Update feedback.php

--- feedback.php
+++ feedback.php
@@ -1,7 +1,3 @@
 <?php
-  if (isset($_POST["name"]) && isset($_POST["message"])) {
-    echo "Thanks for your feedback on Mission In Snowdriftland.";  
-  } else {
-    echo "This endpoint only supports the POST method.";
-  }
+  echo "Thanks for your feedback on Mission In Snowdriftland.";  
 ?>
